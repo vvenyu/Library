7fa872a0fa47da5c204194c4bb7662e04fb04752
vvenyu <232058504+vvenyu@users.noreply.github.com>
2026-03-22 18:03:55 +0100

    Styled book cards

--- script.js
+++ script.js
@@ -2,18 +2,46 @@ const library = [];
 
 //Object constructor
 
-function Book(title, author, pages) {
+function Book(title, author, pages, readed) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.readed = readed;
     this.id = crypto.randomUUID();
 };
 
-function addBookToLibrary() {
-    const book1 = new Book('Atomic Habits', 'James Clear', 300);
-    library.push(book1);
+function addBookToLibrary(title, author, pages, readed) {
+    const book = new Book(title, author, pages, readed);
+    library.push(book);
 };
 
+addBookToLibrary('Atomic Habits', 'James Clear', 300, 'yes');
+addBookToLibrary('book2', 'John Cena', 400, 'no');
 
-addBookToLibrary();
-console.log(library);
+// DOM selectors
+const content = document.querySelector('.content');
+
+
+// Add books to the content
+library.forEach((book) => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    const title = document.createElement('p');
+    const author = document.createElement('p');
+    const pages = document.createElement('p');
+    const readed = document.createElement('p');
+    const svg = document.createElement('svg');
+
+    title.textContent = book.title;
+    author.textContent = book.author;
+    pages.textContent = book.pages;
+    readed.textContent = book.readed;
+
+    card.appendChild(title);
+    card.appendChild(author);
+    card.appendChild(pages);
+    card.appendChild(readed);
+    content.appendChild(card);
+
+});
